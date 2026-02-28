0950ecfeaa83e8238a9c571b9027e58d089d5d3d
torresram <ramirotorres90@yahoo.com.ar>
2026-02-27 21:04:12 -0300

    inicio

---
+++ maxiflix-mvc/Models/Usuarios.cs
@@ -0,0 +1,14 @@
+﻿namespace maxiflix_mvc.Models
+{
+    public class Usuarios
+    {
+        public Guid Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Email { get; set; }
+        public string Clave { get; set; }
+        public DateTime FechaCreacion { get; set; }
+        public bool Activo { get; set; }
+        public string UrlImgPerfil { get; set; }
+    }
+}
--- maxiflix-mvc/obj/Debug/net9.0/maxiflix-mvc.AssemblyInfo.cs
+++ maxiflix-mvc/obj/Debug/net9.0/maxiflix-mvc.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("maxiflix-mvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ff2e7ba761b9ab2567d62807ec571d104b31ed6")]
 [assembly: System.Reflection.AssemblyProductAttribute("maxiflix-mvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("maxiflix-mvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
